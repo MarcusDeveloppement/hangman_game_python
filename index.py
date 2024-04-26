97876eb119e1ba5508823a755036d00fef47a049
Marcus <marcus.frontdev@gmail.com>
2024-04-26 15:30:13 +0200

    add logic in the game

--- index.py
+++ index.py
@@ -5,12 +5,26 @@ secret = random.randint(0,len(words) -1)
 secret_word = words[secret]
 game = {
     "secret_word": secret_word ,
-    "gess_word": "_" * len(secret_word),
+    "guess_word": "_" * len(secret_word),
     "life_point":10,
 }
 
-print(f"{game['gess_word']} | vie: {game['life_point']}")
+print(f"{game['guess_word']} | vie: {game['life_point']}")
 
 while True: 
     letter =input('enter letter: ')
-    print(letter)
+    if letter in game["secret_word"] and letter not in game["guess_word"]:
+        guess_word_list = list(game["guess_word"])
+        for index, current_letter in enumerate(game["secret_word"]):
+            if current_letter == letter:
+                guess_word_list[index] =letter
+        game["guess_word"] = "".join(guess_word_list)
+    elif letter not in game["secret_word"]:
+        game["life_point"]-=1
+    print(f"{game['guess_word']} | vie: {game['life_point']}")
+    if '_' not in game["guess_word"]:
+        print("gagné !") 
+        break
+    elif game["life_point"] <= 0:
+        print("Perdu !")
+        break
